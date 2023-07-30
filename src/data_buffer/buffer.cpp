db223642f76e09b19f1056f9b0eba8865af0e3c5
Ping <li283254184@gmail.com>
2023-07-30 21:40:36 +0800

    update some code

--- src/data_buffer/buffer.cpp
+++ src/data_buffer/buffer.cpp
@@ -1,113 +1,151 @@
 /*
 缓冲区的具体实现，后续的http response要用到缓冲区的内容
- */ 
+*/ 
 #include "buffer.h"
 
-// 缓冲区的构造函数，默认分配可以容纳1024个的char类型元素的vector
-// 写入位置为0，读取位置为0
+/**
+ * @brief 这是buffer的构造函数，构造函数将读位置和写位置都进行初始化，同时定义了缓冲区大小，缓冲区内存储的信息全是字符类型；
+ * @param initBuffSize 初始化的缓冲区大小，默认为1024字节
+*/
 Buffer::Buffer(int initBuffSize) : buffer_(initBuffSize), readPos_(0), writePos_(0) {}
 
-// 还能读取的字符数：
-// 写入位置和读取位置是在不断向前进的，对于读而言，接下来还能读多少，要用写
-// 入到的位置减去已读取到位置；
+
+/**
+ * @brief 获取缓冲区内可读字节数的函数；
+ * @return 写位置和读位置的差，获得了剩余的可读字节数
+*/
 size_t Buffer::ReadableBytes() const {
     return writePos_ - readPos_;
 }
 
-// 还能写的字符数(是字符数)
+/**
+ * @brief 获取缓冲区内还可写的字节数；
+ * @return 写位置和读位置的差，也可以理解为缓冲区内目前剩余的空间；
+*/
 size_t Buffer::WritableBytes() const {
     return buffer_.size() - writePos_;
 }
 
-// 预留的能新写入的字符数；
+/**
+ * @brief 可重利用的空间大小；
+ * @return 读取到的位置
+*/
 size_t Buffer::PrependableBytes() const 
 {
-    /* 
-    * 读位置之前的数据都已经读了，现在这部分空间可以预留给
-    * 新的数据，缓冲区的作用就是开辟一块内存空间，这片空间
-    * 内可以同时进行读写
-    */
-    return readPos_;
+    return readPos_;    // 读位置之前的数据都已经读了，现在这部分空间可以预留给新的数据
 }
 
-// 获取当前读位置的内存实际地址(指针)
+/**
+ * @brief 获取读位置在物理内存中的实际地址
+ * @return 缓冲区的起始内存地址加上读位置的偏移量 
+*/
 const char* Buffer::Peek() const {
     return BeginPtr_() + readPos_;
 }
 
-// 通过参数更新已读取的位置
-// 表明len长度的数据已经读过了
-// 读过的这部分说明已经写入文件了
+/**
+ * @brief 恢复len长度的空间，执行这一步必须要确保len字节的数据确实已读；
+ * @param len 恢复的空间长度
+ */
 void Buffer::Retrieve(size_t len) {
-    assert(len <= ReadableBytes());
+    assert(len <= ReadableBytes()); // 如果长度大于可读，显然就没有读完，是有问题的
     readPos_ += len;
 }
 
-// 给定内存地址end，将读取位置跳转到给定位置
+/**
+ * @brief 恢复直到指定内存地址的空间；
+ * @param end 字符的内存地址，表明内存中该字符之前的所有字符已读；
+ */
 void Buffer::RetrieveUntil(const char* end) {
     assert(Peek() <= end );
     Retrieve(end - Peek());
 }
 
-// 清空缓冲区所有字符
+/**
+ * @brief 清空缓冲区的所有内容，重新初始化读位置和写位置；
+ */
 void Buffer::RetrieveAll() {
-    bzero(&buffer_[0], buffer_.size()); // 将buffer_清零
+    memset(&buffer_[0], 0, buffer_.size()); // 将buffer_清零，这部分的代码我作了更改
     readPos_ = 0;
     writePos_ = 0;
 }
 
-// 将缓冲区中剩余数据拷贝到一个新的str对象中
+/**
+ * @brief 先将缓冲区内还未读取的数据交给一个字符串，然后清空缓冲区；
+ * @return 先前的字符串信息；
+ */
 std::string Buffer::RetrieveAllToStr() {
     std::string str(Peek(), ReadableBytes());
     RetrieveAll();
     return str;
 }
 
-// 获取写入位置处的内存地址，常量版本
+/**
+ * @brief 常量版本，获取写入到的位置的真实内存地址；
+ */
 const char* Buffer::BeginWriteConst() const {
     return BeginPtr_() + writePos_;
 }
 
-// 非常量版本
+/**
+ * @brief 非常量版本，获取写入到的位置的真实内存地址；
+ */
 char* Buffer::BeginWrite() {
     return BeginPtr_() + writePos_;
 }
 
-// 通过参数更新写入位置，这里的len是已经写入的长度
+/**
+ * @brief 确保数据已经写入到了缓冲区之后更新写入位置；
+ * @param len 写入的数据大小；
+*/
 void Buffer::HasWritten(size_t len) {
     writePos_ += len;
 } 
 
-// 以下几个Append函数都是重载函数
-// 参数基本都是const版本，因为我们只是为了在缓冲区添加元素，而无需改动；
-// 参数为string类型时
+/**
+ * @brief 添加内容到缓冲区；
+ * @param str 要添加的字符串地址，由于str的参数是可以确定的，因此只需要一个参数；
+ */
 void Buffer::Append(const std::string& str) {
     Append(str.data(), str.length());
 }
 
-// 参数为指向data数据的指针，且提供要添加的数据长度时
+/**
+ * @brief 重载的函数，添加内容到缓冲区；
+ * @param data 要添加内容，任意类型的常量；
+ * @param len 字符串的长度，该长度应该与上面的内容紧密联系；
+ */
 void Buffer::Append(const void* data, size_t len) {
     assert(data);
     Append(static_cast<const char*>(data), len);    // 任意类型的数据都转化为字符串形式(强制类型转换)
 }
 
-// 参数为字符指针类型时，且提供字符长
+/**
+ * @brief 添加内容到该缓冲区，如果长度不够，再增加本缓冲区的长度，保证大小足够以容纳新的信息；
+ * @param str 要添加的字符串地址；
+ * @param len 字符串的长度，该长度应该与上面的地址紧密联系；
+ */
 void Buffer::Append(const char* str, size_t len) {
     assert(str);    // 确保指针不为空
-    EnsureWriteable(len);
+    EnsureWriteable(len);   // 确保空间足够写入
     std::copy(str, str + len, BeginWrite());    // 将str的指定长度数据复制进以写入位置为起始位置的内存空间
     HasWritten(len);    // 已经写入了len长度的数据
 }
 
-// 参数为另一缓冲区对象的引用时
-// 将另一缓冲区对象(未读取)的数据追加到当前缓冲区中，妙啊
+/**
+ * @brief 添加内容到缓冲区；
+ * @param buff 对另一个缓冲区对象的引用；
+ */
 void Buffer::Append(const Buffer& buff) 
 {
-    // 是不是可以考虑该对象是自己本身的情况
+    // 这里不考虑是本字符串的情况，主要是没有重载等于运算符
     Append(buff.Peek(), buff.ReadableBytes());
 }
 
-// 确保可写入，如果空间不够，则新分配空间；
+/**
+ * @brief 该函数确保缓冲区大小充足，如果不充足则会启用重新分配机制；
+ * @param len 需求的字节大小；
+*/
 void Buffer::EnsureWriteable(size_t len) {
     if(WritableBytes() < len) {
         MakeSpace_(len);
@@ -115,11 +153,14 @@ void Buffer::EnsureWriteable(size_t len) {
     assert(WritableBytes() >= len);
 }
 
-// 该函数想要实现的功能是成功读入文件中的内容到缓冲区
-// 换句话说就是读取文件内容之后，写入到缓冲区
-ssize_t Buffer::ReadFd(int fd, int* saveErrno) {
-    
-    char buff[65535];   // 这是一个临时缓冲区
+/**
+ * @brief 读取文件中的信息到缓冲区；
+ * @param fd 文件描述符信息；
+ * @param saveErrno 指向保存错误码的地址；
+ * @return 成功读取到缓冲区的字节数；
+ */
+ssize_t Buffer::ReadFd(int fd, int* saveErrno) { 
+    char buff[65535];   // 这是一个临时缓冲区，也用来保存字符信息，也用来准备后续的添加操作；
 
     // struct iovec 包含两个成员变量：
     // void* iov_base：指向内存块的起始地址。
@@ -127,11 +168,13 @@ ssize_t Buffer::ReadFd(int fd, int* saveErrno) {
     // iov数组当然是连续的，但是数组中的元素是一个结构体，而其中结构体中的指针指向的
     // 内存是不连续的，这一点要理解
     struct iovec iov[2];    // 分别存储当前对象的缓冲区和buf缓冲区
-    const size_t writable = WritableBytes();    // 当前还可写入的空间大小
+    const size_t writable = WritableBytes();    // 缓冲区当前可写入的空间大小
 
     /* 分散读(内存空间不连续)， 保证数据全部读完 */
+    // 首先是第一个缓冲区，就是我们的缓冲区对象
     iov[0].iov_base = BeginPtr_() + writePos_;  // 写入的起始地址
     iov[0].iov_len = writable;
+    // 下面是第二个缓冲区
     iov[1].iov_base = buff;
     iov[1].iov_len = sizeof(buff);
 
@@ -141,16 +184,19 @@ ssize_t Buffer::ReadFd(int fd, int* saveErrno) {
     const ssize_t len = readv(fd, iov, 2);
     if(len < 0) *saveErrno = errno; // 读取失败则保存错误码到saveErrno中
     else if(static_cast<size_t>(len) <= writable)   writePos_ += len;   // 更新读取位置
-    else 
+    else    // 如果超过了该缓冲区对象的长度
     {
         writePos_ = buffer_.size();     // 长度大于可读取的范围，说明缓冲区已满
-        Append(buff, len - writable);   // 剩余的数据追加到本缓冲区末尾
+        Append(buff, len - writable);   // 剩余的数据追加到本缓冲区，如果空间不足，缓冲区会自动增长空间以满足需求
     }
-    return len;
+    return len; // 确保都处理好了之后，就返回
 }
 
-// 该函数想要实现的功能是将换缓冲区中的内容写入到文件中
-// 换句话说就是读取缓冲区后，将内容写入到文件中
+/**
+ * @brief 将换缓冲区中的内容写入到文件中，换句话说就是将内容写入到文件中，然后更新缓冲区的读位置
+ * @param fd 要保存到的文件描述符；
+ * @param saveErrno 指向保存错误码的地址；
+*/
 ssize_t Buffer::WriteFd(int fd, int* saveErrno)
 {
     size_t readSize = ReadableBytes();  // 还能读取的长度
@@ -166,30 +212,39 @@ ssize_t Buffer::WriteFd(int fd, int* saveErrno)
     return len; // 也是返回写入的字节数
 }
 
-// 字符串起始位置的内存地址
+/**
+ * @brief 获取缓冲区的起始内存地址，这个函数很好的展示了迭代器与指针的区别
+ * @return 首元素的地址值  
+*/
 char* Buffer::BeginPtr_() {
-    return &*buffer_.begin();   // *buffer_.begin()返回的是buffer_首元素的引用，取的是首元素地址
+    return &*buffer_.begin();   // (*buffer_.begin())返回的是buffer_首元素的引用，取的是首元素地址
 }
 
-// const版本
+/** 
+ * @brief 重载版本，获取缓冲区的真实内存地址；
+*/
 const char* Buffer::BeginPtr_() const {
     return &*buffer_.begin();
 }
 
-// 这个函数的目的是保证缓冲区有足够的空间来容纳指定长度len的数据
+/**
+ * @brief 函数保证缓冲区有足够的空间来容纳指定长度为len的数据，该函数实现了缓冲区的自动增长；
+ * @param len 要分配的字节数
+*/
 void Buffer::MakeSpace_(size_t len) 
 {
-    if(WritableBytes() + PrependableBytes() < len)  // 已读取的空间(可重新利用)与可写入数据的空间加起来的长度不够len
-        buffer_.resize(writePos_ + len + 1);        // 重新分配空间，加1是为了考虑'\0'？
+    if(WritableBytes() + PrependableBytes() < len)  // 已读取的空间(可重新利用)与可写入数据的空间加起来的长度不够len，说明不够用
+        buffer_.resize(writePos_ + len + 1);        // 需要重新分配空间，加1是为了给空字符'\0'留一份空间
     else
     {
         size_t readable = ReadableBytes();
 
         // 把剩余未读取的内容重新放到开头
-        // 后续改进可以考虑移动构造函数是吧
+        // 不考虑使用移动构造，移动构造本身处理char类型的字符效率也没好到哪里去
         std::copy(BeginPtr_() + readPos_, BeginPtr_() + writePos_, BeginPtr_());
-        readPos_ = 0;   // 更新读位置
-        writePos_ = readPos_ + readable;    // 更新写位置
-        assert(readable == ReadableBytes());    // 这个数据不会变吧....
+        readPos_ = 0;   // 更新读位置，处理读位置的这一步不会被打断
+        writePos_ = readPos_ + readable;    // 更新写位置(这里有一个细节，即便readPos_更新为0了，还是用它来加，是否有用意)
+        // 如果这边触发了断言，那很有可能是出现了问题，也许是多线程操作导致的
+        assert(readable == ReadableBytes());    // 这个数据按理说不会变，但是由于多线程的原因，可能会导致意外的更改
     }
 }
--- src/data_buffer/buffer.h
+++ src/data_buffer/buffer.h
@@ -1,18 +1,20 @@
 /*
-缓冲区的定义，学习之前需要明确的信息是：
-- read函数读取文件内容，然后这部分内容是写入缓冲区的，也就说针对缓冲区而言是写入行为
+对于缓冲区的处理，学习之前需要明确的信息是：
+- read函数读取文件内容，然后这部分内容是写入缓冲区的，也就说针对缓冲区而言是写入行为，但是为了便于程序员使用我们仍然称为read函数
+    -- 之所以提及到这部分，本质原因是因为，在套接字网络编程中，对于服务端而言，接受从客户端发来的数据就是读。读入到自己的缓冲区
+    -- 客户端同理
 - write函数将缓冲区内容写入文件，相对缓冲区而言是读取的过程 
 
-缓冲区主要用于服务端
+缓冲区本身服务于服务端与客户端之间的数据交换(缓冲区的意义不用多说，OS里面的东西)
  */ 
 
 #ifndef BUFFER_H
 #define BUFFER_H
-#include <cstring>      // perror
+#include <cstring>
 #include <iostream>
 #include <unistd.h>     // write
 #include <sys/uio.h>    // readv
-#include <vector>       // readv
+#include <vector>
 #include <atomic>       // C++11标准中的一些表示线程、并发控制时原子操作的类与方法等
 #include <assert.h>
 class Buffer {
@@ -20,8 +22,8 @@ public:
     Buffer(int initBuffSize = 1024);
     ~Buffer() = default;    // 默认析构
 
-    size_t WritableBytes() const;   // 缓冲区中还可写入的大小      
-    size_t ReadableBytes() const ;  // 缓冲区中可读取的大小
+    size_t WritableBytes() const;
+    size_t ReadableBytes() const ;
     size_t PrependableBytes() const;
 
     const char* Peek() const;
@@ -47,14 +49,15 @@ public:
 
 private:
     char* BeginPtr_();
-    const char* BeginPtr_() const;  // 常量版本
-    void MakeSpace_(size_t len);    // 创建空间
+    const char* BeginPtr_() const;
+    void MakeSpace_(size_t len);
 
-    std::vector<char> buffer_;          // 定义了一串字符串
+    std::vector<char> buffer_;          // 字符串数组
 
     // 为什么要保证原子性呢，我的理解是，不同Buffer对象可对该位置进行改动
     // 为了防止某对象改动的时候，被其他需要改动的对象破坏了这个过程
     // 以至于该对象没有按照预期改动这个位置
+    // 这是C++ 11标准以后的内容
     std::atomic<std::size_t> readPos_;  // 定义了读取到的位置，atomic保证原子性
     std::atomic<std::size_t> writePos_; // 定义了写入到的位置，保证原子性
 };
