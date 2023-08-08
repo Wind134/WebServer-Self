8f8ae5c02da82dc9015f4d9dd9ebb4be1f360e56
Ping <li283254184@gmail.com>
2023-08-08 21:48:27 +0800

    organize the code

--- src/http/httpresponse.cpp
+++ src/http/httpresponse.cpp
@@ -1,11 +1,16 @@
 /*
-HTTP回应的头文件
- */ 
+- HTTP响应报文处理的实现；
+- 响应报文是服务器发给客户端的；
+- 而请求报文是客户端发给服务器的；
+- 这里用到了文件映射机制，即将文件中的内容全盘映射到内存；
+*/ 
 #include "httpresponse.h"
-
 using namespace std;
 
-const unordered_map<string, string> HttpResponse::SUFFIX_TYPE = {   // 定义文件后缀->类型的映射
+/**
+ * @brief 文件后缀对应的文件类型的映射，文件类型的数据信息来源于请求报文的请求头部；
+ */
+const unordered_map<string, string> HttpResponse::SUFFIX_TYPE = {
     { ".html",  "text/html" },
     { ".xml",   "text/xml" },
     { ".xhtml", "application/xhtml+xml" },
@@ -27,19 +32,27 @@ const unordered_map<string, string> HttpResponse::SUFFIX_TYPE = {   // 定义文
     { ".js",    "text/javascript "},
 };
 
-const unordered_map<int, string> HttpResponse::CODE_STATUS = {      // 映射错误码的具体内容
-    { 200, "OK" },
+/**
+ * @brief 状态码-->服务器状态的映射；
+*/
+const unordered_map<int, string> HttpResponse::CODE_STATUS = {
     { 400, "Bad Request" },
     { 403, "Forbidden" },
     { 404, "Not Found" },
 };
 
-const unordered_map<int, string> HttpResponse::CODE_PATH = {        // 错误类型与网页路径的映射
+/**
+ * @brief 状态码-->展示给用户的界面网页路径的映射；
+*/
+const unordered_map<int, string> HttpResponse::CODE_PATH = {
     { 400, "/400.html" },
     { 403, "/403.html" },
     { 404, "/404.html" },
 };
 
+/**
+ * @brief 这是一个构造函数，初始化http相应处理中的一系列变量；
+ */
 HttpResponse::HttpResponse() {
     code_ = -1;             // 错误码默认定义为-1                
     path_ = srcDir_ = "";   // 路径
@@ -48,13 +61,23 @@ HttpResponse::HttpResponse() {
     mmFileStat_ = { 0 };    // 结构体的初始化语法，状态初始化为0
 };
 
+/**
+ * @brief 析构函数的工作就是解除文件映射；
+ */
 HttpResponse::~HttpResponse() {
     UnmapFile();
 }
 
+/**
+ * @brief 发送响应报文阶段的初始化操作；
+ * @param srcDir 资源文件目录的地址；
+ * @param path 资源文件的地址(以srcDir为根目录)；
+ * @param isKeepAlive 是否持久连接；
+ * @param code 状态码；
+ */
 void HttpResponse::Init(const string& srcDir, string& path, bool isKeepAlive, int code){
     assert(srcDir != "");       // 既然是初始化，那么要求srcDir不为空
-    if(mmFile_) { UnmapFile(); }// 如果指向的内容不为空，那么先将指向的内容释放
+    if(mmFile_) { UnmapFile(); }// 如果指向的内容不为空(有数据)，那么先将指向的内容释放
     code_ = code;
     isKeepAlive_ = isKeepAlive; // 跟着初始化设定的参数走
     path_ = path;               
@@ -63,17 +86,21 @@ void HttpResponse::Init(const string& srcDir, string& path, bool isKeepAlive, in
     mmFileStat_ = { 0 };
 }
 
+/**
+ * @brief 向客户端发送响应报文；
+ * @param buff 向缓冲区写入响应报文，buff是写入的目标缓冲区；
+ */
 void HttpResponse::MakeResponse(Buffer& buff) {
     /* 判断请求的资源文件 */
     // string的data函数返回一个底层字符串指针，这段字符串会传进指向mmFileStat_变量的地址
-    // 如果获取失败，或者获取到的文件信息是一个目录，那么返回404
+    // 如果stat的返回值小于0，那么表明获取失败，或者获取到的文件信息是一个目录，那么返回404(没找到)
     if(stat((srcDir_ + path_).data(), &mmFileStat_) < 0 || S_ISDIR(mmFileStat_.st_mode)) {
         code_ = 404;
     }
-    else if(!(mmFileStat_.st_mode & S_IROTH)) { // 如果其他用户没有访问权限，则错误码设定为403
+    else if(!(mmFileStat_.st_mode & S_IROTH)) { // 如果其他用户没有(读)访问权限，则错误码设定为403
         code_ = 403;
     }
-    else if(code_ == -1) { // code还是-1，表明没有发生任何错误，那就将错误码设定为200
+    else if(code_ == -1) { // code还是初始设定的-1，表明没有发生任何错误，那就将错误码设定为200
         code_ = 200; 
     }
     ErrorHtml_();
@@ -82,14 +109,25 @@ void HttpResponse::MakeResponse(Buffer& buff) {
     AddContent_(buff);
 }
 
+/**
+ * @brief 获取文件在内存中的地址；
+ * @return 文件内容的内存首地址；
+ */
 char* HttpResponse::File() {
     return mmFile_;
 }
 
+/**
+ * @brief 获取文件的大小；
+ * @return 文件的大小；
+ */
 size_t HttpResponse::FileLen() const {
     return mmFileStat_.st_size; // 返回文件大小，默认单位是字节
 }
 
+/**
+ * @brief 该函数的功能是将状态码与要展示的HTML文件路径对应起来；
+ */
 void HttpResponse::ErrorHtml_() {
     if(CODE_PATH.count(code_) == 1) {   // 如果三个错误码中存在某一个
         path_ = CODE_PATH.find(code_)->second;  // 获取路径
@@ -97,32 +135,43 @@ void HttpResponse::ErrorHtml_() {
     }
 }
 
+/**
+ * @brief 将HTTP的响应报文中的状态行添加到服务器的缓冲区；
+ * @param buff 服务器缓冲区的引用；
+ */
 void HttpResponse::AddStateLine_(Buffer& buff) {
     string status;  // 错误码标识的具体状态
     if(CODE_STATUS.count(code_) == 1) { // 只要返回了code_信息，则标识状态
         status = CODE_STATUS.find(code_)->second;
     }
     else {  // 如果没有返回，表明request部分有问题
-        code_ = 400;    // 此时更新错误码为400
+        code_ = 400;    // 此时更新错误码为400，表明"Bad Request!"
         status = CODE_STATUS.find(400)->second; // 更新状态
     }
-    buff.Append("HTTP/1.1 " + to_string(code_) + " " + status + "\r\n");    // 将这段内容加入缓冲区
+    buff.Append("HTTP/1.1 " + to_string(code_) + " " + status + "\r\n");    // 将这段内容加入缓冲区(包括换行)
 }
 
+/**
+ * @brief 将响应头部内容添加到缓冲区；
+ * @param buff 服务器缓冲区的引用；
+ */
 void HttpResponse::AddHeader_(Buffer& buff) {
     buff.Append("Connection: ");
     if(isKeepAlive_) {  // 如果是持久连接
         buff.Append("keep-alive\r\n");
-        buff.Append("keep-alive: max=6, timeout=120\r\n");  // 单个连接的最大请求数量是6，120s内没有发出新的请求则关闭
+        buff.Append("keep-alive: max=6, timeout=120\r\n");  // 持久连接的单个连接的最大请求数量是6，120s内没有发出新的请求则关闭
     } else{ // 如果不是持久连接，则写入close信息
         buff.Append("close\r\n");
     }
     buff.Append("Content-type: " + GetFileType_() + "\r\n");
 }
 
+/**
+ * @brief 将网页文件的内容写入到缓冲区(这里只添加了响应头部字段中的Content-length属性)；
+ */
 void HttpResponse::AddContent_(Buffer& buff) {
-    int srcFd = open((srcDir_ + path_).data(), O_RDONLY);   // 获取网页内容，写入缓冲区
-    if(srcFd < 0) { 
+    int srcFd = open((srcDir_ + path_).data(), O_RDONLY);   // 以只读方式打开网页文件
+    if(srcFd < 0) { // 文件打开失败，则向内容中写入具体的错误信息；
         ErrorContent(buff, "File NotFound!");
         return; 
     }
@@ -130,7 +179,7 @@ void HttpResponse::AddContent_(Buffer& buff) {
     // 将文件映射到内存提高文件的访问速度 
     // MAP_PRIVATE 建立一个写入时拷贝的私有映射
     // 也就是对内存内容的修改不会影响文本本身的内容
-    LOG_DEBUG("file path %s", (srcDir_ + path_).data());
+    LOG_DEBUG("file path %s", (srcDir_ + path_).data());    // 在日志上打印网页文件的具体路径信息
     int* mmRet = (int*)mmap(0, mmFileStat_.st_size, PROT_READ, MAP_PRIVATE, srcFd, 0);
     if(*mmRet == -1) {
         ErrorContent(buff, "File NotFound!");
@@ -138,9 +187,12 @@ void HttpResponse::AddContent_(Buffer& buff) {
     }
     mmFile_ = (char*)mmRet; // 将内容映射到了内存，并转为字符串格式
     close(srcFd);
-    buff.Append("Content-length: " + to_string(mmFileStat_.st_size) + "\r\n\r\n");
+    buff.Append("Content-length: " + to_string(mmFileStat_.st_size) + "\r\n\r\n");  // 响应头与响应体之间有一空行；
 }
 
+/**
+ * @brief 解除文件在内存中的映射；
+ */
 void HttpResponse::UnmapFile() {
     if(mmFile_) {
         munmap(mmFile_, mmFileStat_.st_size);   // 解除文件映射的函数，其中第二个参数是文件大小
@@ -148,6 +200,10 @@ void HttpResponse::UnmapFile() {
     }
 }
 
+/**
+ * @brief 根据后缀来判断文件类型；
+ * @return 返回具体的文件类型；
+ */
 string HttpResponse::GetFileType_() {
     /* 判断文件类型 */
     string::size_type idx = path_.find_last_of('.');
@@ -161,21 +217,26 @@ string HttpResponse::GetFileType_() {
     return "text/plain";    // 否则默认还是返回纯文本格式
 }
 
+/**
+ * @brief 将错误内容写入到缓冲区，倒不是直接写入错误信息，而是会加入一些HTML标签；
+ * @param message 具体的错误信息；
+ */
 void HttpResponse::ErrorContent(Buffer& buff, string message) 
 {
     string body;
     string status;
+    // 如果所料不差，那么下面展示的应该就是平时遇到的那一大串无情的空白页面在左上角附带一个冰冷的文字；
     body += "<html><title>Error</title>";
     body += "<body bgcolor=\"ffffff\">";
-    if(CODE_STATUS.count(code_) == 1) { 
+    if(CODE_STATUS.count(code_) == 1) {     // 如果是4字段的状态码，则更新其状态信息
         status = CODE_STATUS.find(code_)->second;
-    } else {    // http响应端无法接受请求
-        status = "Bad Request";
+    } else {
+        status = "Bad Request"; // 如果是其他状态则更新为Bad Request
     }
     body += to_string(code_) + " : " + status  + "\n";
     body += "<p>" + message + "</p>";
-    body += "<hr><em>TinyWebServer</em></body></html>";
+    body += "<hr><em>WebServer</em></body></html>";
 
-    buff.Append("Content-length: " + to_string(body.size()) + "\r\n\r\n");
+    buff.Append("Content-length: " + to_string(body.size()) + "\r\n\r\n");  // 细节，在添加响应体的时候给了一个空格字符；
     buff.Append(body);
 }
--- src/http/httpresponse.h
+++ src/http/httpresponse.h
@@ -1,5 +1,15 @@
 /*
-HTTP响应的头文件，封装了http响应环节的诸多方法
+- 封装了处理HTTP响应报文的头文件；
+- 封装了处理http响应报文环节的诸多方法；
+- 涉及到的一些有用的知识：
+    -- 在C/C++中，保存文件元数据的结构体 'struct stat' 用于存储有关文件的信息的数据结构，包括：
+        --- st_mode：文件的类型和访问权限。
+        --- st_size：文件的大小（以字节为单位）。
+        --- st_atime：文件的最后访问时间。
+        --- st_mtime：文件的最后修改时间。
+        --- st_ctime：文件的最后状态更改时间。
+    -- 内存映射机制，允许将一个文件或其他资源映射到进程的地址空间，使得可以通过指针对其进行读取和写入，就像访问内存一样。 
+        --- 实现内存映射的函数：void *mmap()       
  */ 
 #ifndef HTTP_RESPONSE_H
 #define HTTP_RESPONSE_H
@@ -10,18 +20,17 @@ HTTP响应的头文件，封装了http响应环节的诸多方法
 #include <sys/stat.h>    // 访问文件状态
 #include <sys/mman.h>    // 内存映射相关
 
-#include "../data_buffer/buffer.h"
-#include "../log_system/log.h"
+#include "../data_buffer/buffer.h"  // 缓冲池
+#include "../log_system/log.h"      // 日志系统
 
 class HttpResponse {
 public:
     HttpResponse();     // 构造函数
     ~HttpResponse();    // 析构函数
 
-    // 初始化http连接响应
+    
     void Init(const std::string& srcDir, std::string& path, bool isKeepAlive = false, int code = -1);
     
-    // 建立http响应
     void MakeResponse(Buffer& buff);
     
     // 该函数用来解除文件映射
@@ -38,18 +47,16 @@ public:
     int Code() const { return code_; }
 
 private:
-    // 添加状态行，信息写入内存缓冲区
+
     void AddStateLine_(Buffer &buff);
 
-    // 添加Header内容进缓冲区，即连接信息写入缓冲区
     void AddHeader_(Buffer &buff);
 
     // 将网页内容放入缓冲区
     void AddContent_(Buffer &buff);
 
-    // 将错误码与展示的网页路径对应起来
     void ErrorHtml_();
-    // 获取文件类型
+    
     std::string GetFileType_();
 
     int code_;              // 定义的应该是错误码
@@ -62,9 +69,9 @@ private:
     struct stat mmFileStat_;    // 这是保存文件元数据的结构体
 
     // static变量声明，将文件后缀与文件类型相互对应的映射
+    // 这里有一个C++的小知识点，静态成员变量不能再类内进行初始化；
     static const std::unordered_map<std::string, std::string> SUFFIX_TYPE;
 
-    // 错误类型->错误内容的映射
     static const std::unordered_map<int, std::string> CODE_STATUS;
 
     // 错误类型->网页展示路径的映射
